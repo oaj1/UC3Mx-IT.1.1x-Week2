eb5327d03c11bf87665b51ce995c2d8014d56d44
Oliver James <31637093+oaj1@users.noreply.github.com>
2018-01-01 07:03:27 -0600

    Add files via upload

---
+++ whileLoop.java
@@ -0,0 +1,26 @@
+/**
+ * Do not change the method or class
+ * declarations.
+ */
+ 
+public class whileLoop {
+
+	public static void main(String[] args) {
+	
+	    int nOne = 2;
+	    int nTwo = 3;
+	    int result = 1;
+	    int limit = 1000;
+	    
+	    while(result < limit){
+	        System.out.println(result);
+	        result = nOne * nTwo;
+	        nOne++;
+	        nTwo = nTwo *2;
+	       if (nTwo >= 50){
+	           break;
+	       }
+	    }
+	
+	}
+}
